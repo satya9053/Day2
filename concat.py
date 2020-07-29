7391f1e756aad30153d85c2873d59670e14a3981
satya9053 <68903208+satya9053@users.noreply.github.com>
2020-07-29 08:30:36 +0530

    Add files via upload

---
+++ concat.py
@@ -0,0 +1,11 @@
+
+firstname = input("Enter the first name")
+lastname = input("Enter the second name")
+username = fname + sname
+pass1 = input("set password")
+print(fullname)
+pwd = input("enter the password to log on")
+if pwd == pass1:
+    print(fullname + " is logged")
+else:
+    print("password is incorrect")
